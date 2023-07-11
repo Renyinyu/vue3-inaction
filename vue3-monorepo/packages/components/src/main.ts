7de52a9a3f3627d253da8be64053c4c6dff35d3c
chenjian <471630805@qq.com>
2023-07-12 00:04:51 +0800

    chore: build component

--- vue3-monorepo/packages/components/src/main.ts
+++ vue3-monorepo/packages/components/src/main.ts
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
 
-import Example from '../example/Example.vue'
+// import Example from '../example/Example.vue'
 
-const app = createApp(Example)
+// const app = createApp(Example)
 
-app.mount('#app')
+// app.mount('#app')
+
+export const app = 'vue app example components'
 
--- vue3-monorepo/packages/components/vite.config.ts
+++ vue3-monorepo/packages/components/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
+import { name } from './package.json'
 
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx()
-  ]
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: name,
+      fileName: 'my-components'
+    }
+  }
 })
